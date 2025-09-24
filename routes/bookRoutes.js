e13b65f3ea2eda411026f1129ad31feb7bb30428
DSM3 <fatec-dsm3@academico.intra>
2025-09-23 22:05:27 -0300

    @AnandaMazine

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,15 +2,16 @@ import express from "express";
 const bookRoutes = express.Router();
 
 import bookController from "../controllers/bookController.js";
+import Auth from "../middleware/Auth.js";
 
-bookRoutes.get("/books", bookController.getAllBook);
+bookRoutes.get("/books", Auth.Authorization,bookController.getAllBook);
 
-bookRoutes.post("/books", bookController.createBook);
+bookRoutes.post("/books", Auth.Authorization, bookController.createBook);
 
-bookRoutes.delete("/books/:id", bookController.deleteBook);
+bookRoutes.delete("/books/:id", Auth.Authorization, bookController.deleteBook);
 
-bookRoutes.put("/books/:id", bookController.updateBook);
+bookRoutes.put("/books/:id", Auth.Authorization, bookController.updateBook);
 
-bookRoutes.get("/books/:id", bookController.getOneBook);
+bookRoutes.get("/books/:id", Auth.Authorization, bookController.getOneBook);
 
 export default bookRoutes;
